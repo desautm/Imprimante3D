2150a369e68e3173f1a26ef70f51ac995ae01c21
Marc-André Désautels <desautm@hotmail.com>
2022-05-13 13:41:32 -0400

    Time to stop...

--- R/csg.R
+++ R/csg.R
@@ -1,4 +1,5 @@
 library(tidyverse)
+library(latex2exp)
 
 poly <- tribble(
   ~x, ~y, ~holes, ~id,
@@ -53,8 +54,17 @@ poly_trou <- tribble(
 )
 
 b1 <- poly[c(1:3),]
-b2 <- poly[c(3:10),]
-b3 <- poly[c(12,13,14,15),]
+b3 <- poly[c(3:10),]
+b2 <- poly[c(12,13,14,15),]
+
+c1 <- poly[c(4,5,6),]
+c2 <- poly[c(6,7,8,9),]
+
+c_b3 <- b3 %>% 
+  slice(chull(x,y))
+
+not_c_b3 <- b3 %>% 
+  slice(-chull(x,y))
   
 p1 <- ggplot() +
   aes(x=x,y=y)+
@@ -105,6 +115,14 @@ p2 <- ggplot() +
 p3 <- ggplot() +
   aes(x=x,y=y)+
   geom_polygon(
+    data=poly_plein,
+    aes(subgroup=holes),
+    color = "black", 
+    fill = "#3b528b",
+    alpha = 0.5,
+    size=1.5
+  )+
+  geom_polygon(
     data=c_poly_plein,
     aes(subgroup=holes),
     color = "black", 
@@ -154,11 +172,110 @@ p4 <- ggplot() +
   geom_point(data=poly_plein, size=5)+
   theme_void()
 
+p5 <- ggplot() +
+  aes(x=x,y=y)+
+  geom_polygon(
+    data=poly_plein,
+    aes(subgroup=holes),
+    color = "gray", 
+    fill = "#3b528b",
+    alpha = 0,
+    size=1.5
+  )+
+  geom_polygon(
+    data=b1,
+    color = "black", 
+    fill = "#fde725",
+    alpha = 0.5,
+    size=1.5
+  )+
+  geom_polygon(
+    data=b2,
+    color = "black", 
+    fill = "#fde725",
+    alpha = 0.5,
+    size=1.5
+  )+
+  geom_polygon(
+    data=b3,
+    color = "black", 
+    fill = "#fde725",
+    alpha = 0.5,
+    size=1.5
+  )+
+  annotate("text", x=0.1709, y=0.7677, label=TeX("$B_1$"), size=6)+
+  annotate("text", x=0.3009, y=0.4416, label=TeX("$B_3$"), size=6)+
+  annotate("text", x=0.7272, y=0.6228, label=TeX("$B_2$"), size=6)+
+  geom_point(data=poly_plein, size=5)+
+  theme_void()
 
-
-
+p6 <- ggplot() +
+  aes(x=x,y=y)+
+  geom_polygon(
+    data=poly_plein,
+    aes(subgroup=holes),
+    color = "gray", 
+    fill = "#3b528b",
+    alpha = 0,
+    size=1.5
+  )+
+  geom_polygon(
+    data=b3,
+    color = "black", 
+    fill = "#fde725",
+    alpha = 0.5,
+    size=1.5
+  )+
+  annotate("text", x=0.3009, y=0.4416, label=TeX("$B_3$"), size=12)+
+  geom_point(data=poly_plein, size=5)+
+  theme_void()
   
+p7 <- ggplot() +
+  aes(x=x,y=y)+
+  geom_polygon(
+    data=poly_plein,
+    aes(subgroup=holes),
+    color = "gray", 
+    fill = "#3b528b",
+    alpha = 0,
+    size=1.5
+  )+
+  geom_polygon(
+    data=c_b3,
+    color = "black", 
+    fill = "#5ec962",
+    alpha = 0.5,
+    size=1.5
+  )+
+  annotate("text", x=0.3009, y=0.4416, label=TeX("$C$"), size=12)+
+  geom_point(data=poly_plein, size=5)+
+  theme_void()
   
-  
-
-
+p8 <- ggplot() +
+  aes(x=x,y=y)+
+  geom_polygon(
+    data=poly_plein,
+    aes(subgroup=holes),
+    color = "gray", 
+    fill = "#3b528b",
+    alpha = 0,
+    size=1.5
+  )+
+  geom_polygon(
+    data=c1,
+    color = "black", 
+    fill = "#fde725",
+    alpha = 0.5,
+    size=1.5
+  )+
+  geom_polygon(
+    data=c2,
+    color = "black", 
+    fill = "#fde725",
+    alpha = 0.5,
+    size=1.5
+  )+
+  annotate("text", x=0.3009, y=0.58, label=TeX("$D_1$"), size=6)+
+  annotate("text", x=0.43, y=0.4, label=TeX("$D_2$"), size=6)+
+  geom_point(data=poly_plein, size=5)+
+  theme_void()
